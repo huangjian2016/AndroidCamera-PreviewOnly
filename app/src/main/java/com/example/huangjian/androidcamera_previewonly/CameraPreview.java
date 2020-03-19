8eb65f1c916d5a7a42713348c9f243ce3a5c3a0e
huangjian2016 <1186768453@qq.com>
2020-03-19 16:18:13 +0800

    Description: 提交相机偏好设置 UI 功能 Cause: 提交相机偏好设置 UI 功能 Solution: 提交相机偏好设置 UI 功能 Reviewed: huangjian

--- app/src/main/java/com/example/huangjian/androidcamera_previewonly/CameraPreview.java
+++ app/src/main/java/com/example/huangjian/androidcamera_previewonly/CameraPreview.java
@@ -19,7 +19,8 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         mHolder.addCallback(this);
     }
 
-    private static Camera getCameraInstance() {
+    public  Camera getCameraInstance() {
+        /*
         Camera c = null;
         try {
             c = Camera.open();
@@ -27,10 +28,20 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
             Log.d(TAG, "camera is not available");
         }
         return c;
+        */
+        if(mCamera == null){
+            try{
+                mCamera = Camera.open();
+            } catch(Exception e){
+                Log.d(TAG,"camera is not available");
+            }
+        }
+        return mCamera;
     }
 
     public void surfaceCreated(SurfaceHolder holder) {
-        mCamera = getCameraInstance();
+        //mCamera = getCameraInstance();
+        getCameraInstance();
         try {
             mCamera.setPreviewDisplay(holder);
             mCamera.startPreview();
--- app/src/main/java/com/example/huangjian/androidcamera_previewonly/MainActivity.java
+++ app/src/main/java/com/example/huangjian/androidcamera_previewonly/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.huangjian.androidcamera_previewonly;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +17,23 @@ public class MainActivity extends AppCompatActivity {
         CameraPreview mPreview=new CameraPreview(this);
         FrameLayout preview=(FrameLayout)findViewById(R.id.camera_preview);
         preview.addView(mPreview);
+
+        SettingsFragment.passCamera(mPreview.getCameraInstance());
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+        SettingsFragment.setDefault(PreferenceManager.getDefaultSharedPreferences(this));
+        SettingsFragment.init(PreferenceManager.getDefaultSharedPreferences(this));
+
+        Button buttonSettings = (Button) findViewById(R.id.button_settings);
+        buttonSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.camera_preview, new SettingsFragment()).addToBackStack(null).commit();
+            }
+        });
+    }
+
+    public void onPause() {
+        finish();
+        super.onPause();
     }
 }
